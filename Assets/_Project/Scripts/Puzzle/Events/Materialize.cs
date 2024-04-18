19cebec39f2b5ac117412df2f65ad448052cc788
arenonesf <arenonesf@gmail.com>
2024-04-18 15:17:25 +0200

    Fix materialize

--- Assets/_Project/Scripts/Puzzle/Events/Materialize.cs
+++ Assets/_Project/Scripts/Puzzle/Events/Materialize.cs
@@ -1,10 +1,8 @@
-using System.Collections;
 using ProjectBPop.Interfaces;
 using UnityEngine;
 
 public class Materialize : MonoBehaviour, IReact
 { 
-    [SerializeField] private float fadeStep = 0.1f;
     private MeshRenderer _meshRenderer;
     private float _colorAlpha;
     private Material _material;
@@ -20,20 +18,7 @@ public class Materialize : MonoBehaviour, IReact
         color.a = .4f;
         _meshRenderer.materials[0].color = color;
     }
-
-    private void SetShader()
-    {
-        _meshRenderer.material.SetFloat("_Mode", 2);
-        _meshRenderer.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        _meshRenderer.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        _meshRenderer.material.SetInt("_ZWrite", 0);
-        _meshRenderer.material.DisableKeyword("_ALPHATEST_ON");
-        _meshRenderer.material.EnableKeyword("_ALPHABLEND_ON");
-        Material material;
-        (material = _meshRenderer.material).DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        material.renderQueue = 3000;
-    }
-
+    
     public void React(bool solve)
     {
         if (solve)

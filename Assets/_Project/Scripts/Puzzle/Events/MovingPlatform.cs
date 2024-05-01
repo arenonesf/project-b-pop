3819819d1ed5d3b4210e96ed8a5df5a58da723de
arenonesf <arenonesf@gmail.com>
2024-05-01 22:07:39 +0200

    Two types of moving platforms

--- Assets/_Project/Scripts/Puzzle/Events/MovingPlatform.cs
+++ Assets/_Project/Scripts/Puzzle/Events/MovingPlatform.cs
@@ -3,66 +3,51 @@ using UnityEngine;
 
 public class MovingPlatform : Mechanism
 {
-    [SerializeField] private PlatformPath waypointPath;
     [SerializeField] private float speed;
-    [SerializeField] private bool loop;
+    private bool _shouldOpen;
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform origin;
+    private Vector3 _currentTarget;
+    private float _currentSpeed;
 
-    private int _targetWaypointIndex;
-    private Transform _previousWaypoint;
-    private Transform _targetWaypoint;
-    private float _timeToWaypoint;
-    private float _elapsedTime;
-
-    private void Start()
+    private void Update()
     {
-        TargetNextWaypoint();
+        if (!_shouldOpen) return;
+        Move(_currentTarget);
     }
 
-    private void FixedUpdate()
+    private void MoveToDesiredTarget()
     {
-        if (!Solved) return;
-        _elapsedTime += Time.deltaTime;
+        _shouldOpen = true;
+        _currentTarget = target.position;
+        _currentSpeed = speed;
+    }
 
-        var elapsedPercentage = _elapsedTime / _timeToWaypoint;
-        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);
-        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
-        transform.rotation = Quaternion.Lerp(_previousWaypoint.rotation, _targetWaypoint.rotation, elapsedPercentage);
-
-        if (elapsedPercentage >= 1 && loop)
+    private void Move(Vector3 newTarget)
+    {
+        var direction = newTarget - transform.position;
+        direction.Normalize();
+        transform.Translate(direction * (_currentSpeed * Time.deltaTime));
+        if (Vector3.Distance(transform.position, newTarget) <= 0.1f)
         {
-            TargetNextWaypoint();
+            _currentSpeed = 0;
         }
-    }
-
-    private void TargetNextWaypoint()
-    {
-        _previousWaypoint = waypointPath.GetWaypoint(_targetWaypointIndex);
-        _targetWaypointIndex = waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
-        _targetWaypoint = waypointPath.GetWaypoint(_targetWaypointIndex);
-
-        _elapsedTime = 0;
-
-        var distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
-        _timeToWaypoint = distanceToWaypoint / speed;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        other.transform.SetParent(transform);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        other.transform.SetParent(null);
     }
     
     public override void Activate()
     {
-        Solved = true;
+        MoveToDesiredTarget();
     }
 
     public override void Deactivate()
     {
-        Solved = false;
+        ReturnToOriginalPosition();
+    }
+
+    private void ReturnToOriginalPosition()
+    {
+        _shouldOpen = true;
+        _currentTarget = origin.position;
+        _currentSpeed = speed;
     }
 }

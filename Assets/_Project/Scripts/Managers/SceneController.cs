f9c3f2be1fa4e1341f8d58d02af14181ed4877e9
arenonesf <arenonesf@gmail.com>
2024-05-11 17:29:32 +0200

    Reapply "Add Bootstrapper"
    This reverts commit a5bca9ef871b6824ebd0dd725da156bfb69a10fe.

--- Assets/_Project/Scripts/Managers/SceneController.cs
+++ Assets/_Project/Scripts/Managers/SceneController.cs
@@ -15,7 +15,6 @@ public partial class SceneController : MonoBehaviour
         }
 
         Instance = this;
-        DontDestroyOnLoad(Instance);
     }
 
     public void LoadScene(SceneReference scene)

d7cae947ae6aeed01927b4cc1d66c5dc15876ad9
arenonesf <arenonesf@gmail.com>
2024-05-13 17:33:06 +0200

    Refactor: magic artifact, nodes and sources

--- Assets/_Project/Scripts/Magic Source/MagicArtifact.cs
+++ Assets/_Project/Scripts/Magic Source/MagicArtifact.cs
@@ -5,6 +5,7 @@ public abstract class MagicArtifact : MonoBehaviour
     [SerializeField] protected bool active;
     [SerializeField] protected SourceType acceptType;
     [SerializeField] protected SourceType type;
+    public bool Active => active;
 
     protected abstract void RetrieveMagic();
     protected abstract void SendMagic();

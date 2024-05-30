ee71bccdfe04264652ed3c522cc7d98422cda582
Zalgazor <asaluena@edu.tecnocampus.cat>
2024-05-30 11:25:36 +0200

    Doors and plattforms emissive

--- Assets/_Project/Scripts/Materials/StatesDecalEmision/StateDecalIdleActivated.cs
+++ Assets/_Project/Scripts/Materials/StatesDecalEmision/StateDecalIdleActivated.cs
@@ -12,7 +12,7 @@ public class StateDecalIdleActivated : IState
     }
     public void OnEnter()
     {
-        _blackboard.Intensity = 10f;
+        _blackboard.Intensity = _blackboard.MaxIntensity;
         _blackboard.Material.SetVector("_EmissionColor", _blackboard.EmissionColorValue * _blackboard.Intensity);
         _blackboard.Material.color = new Color(_blackboard.Material.color.r, _blackboard.Material.color.g, _blackboard.Material.color.b, 1);
     }
--- Assets/_Project/Scripts/Materials/StatesMagicArtifactsEmision/StateActivateEmision.cs
+++ Assets/_Project/Scripts/Materials/StatesMagicArtifactsEmision/StateActivateEmision.cs
@@ -16,12 +16,12 @@ public class StateActivateEmision : IState
     }
     public void OnUpdate()
     {
-        _blackboard.Intensity = Mathf.Lerp(_blackboard.Intensity, 10, Time.deltaTime * _blackboard.ActivateEmisionSpeed);
+        _blackboard.Intensity = Mathf.Lerp(_blackboard.Intensity, _blackboard.MaxIntensity, Time.deltaTime * _blackboard.ActivateEmisionSpeed);
         _blackboard.Material.SetVector("_EmissionColor", _blackboard.EmissionColorValue * _blackboard.Intensity);
     }
     public void OnExit()
     {
-        _blackboard.Intensity = 10f;
+        _blackboard.Intensity = _blackboard.MaxIntensity;
         _blackboard.Material.SetVector("_EmissionColor", _blackboard.EmissionColorValue * _blackboard.Intensity);
     }
 }
